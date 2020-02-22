5afc7112e45290ad9bce83808f3c902a9194fcb9
eric5800602 <eric5800602@gmail.com>
2020-02-22 15:48:54 +0800

    Change to use natural sort and add new trace file

--- traces/trace-natsort.cmd
+++ traces/trace-natsort.cmd
@@ -1,28 +1,16 @@
 new
-it pic02000
-it pic05
-it pic2
-it pic120
-it pic121
-it tom
-it x2-g8
-it x2-y08
-it x2-y7
-it x8-y8
-it 1-02
-it 1-2
-it 1-20
-it 10-20
-it fred
-it jane
-it pic01
-it pic02
-it pic02a
-it pic3
-it pic4
-it pic100
-it pic100a
+ih pic02000
+ih pic05
+ih pic2
+ih pic120
+ih pic121
+ih pic01
+ih pic02
+ih pic02a
+ih pic3
+ih pic4
+ih pic100
+ih pic100a
 sort
-show
 free
 quit
