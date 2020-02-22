328f90ea9a97ddb883f2b3833672a396a44eb045
eric5800602 <eric5800602@gmail.com>
2020-02-22 12:30:50 +0800

    Include natural sort and delete useless comments

---
+++ traces/trace-natsort.cmd
@@ -0,0 +1,28 @@
+new
+it pic02000
+it pic05
+it pic2
+it pic120
+it pic121
+it tom
+it x2-g8
+it x2-y08
+it x2-y7
+it x8-y8
+it 1-02
+it 1-2
+it 1-20
+it 10-20
+it fred
+it jane
+it pic01
+it pic02
+it pic02a
+it pic3
+it pic4
+it pic100
+it pic100a
+sort
+show
+free
+quit
